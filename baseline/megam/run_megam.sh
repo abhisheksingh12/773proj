23a5d16200483bf97d0fc261cedb8fe946da4477
Wu, Ke <wuke@cs.umd.edu>
2011-04-06 23:13:39 -0400

    tuning and testing scrips

---
+++ baseline/megam/run_megam.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+export PATH="`pwd`/../../tools:$PATH"
+
+# prepare input
+tmp="megam-run"
+cp train.megam "$tmp"
+echo "DEV" >> "$tmp"
+cat dev.megam >> "$tmp"
+echo "TEST" >> "$tmp"
+cat test.megam >> "$tmp"
+
+# training
+megam -maxi 500 -lambda 100 -tune multiclass "$tmp" > "$tmp.weights"
+# testing
+megam -predict "$tmp.weights" multiclass test.megam | grep -oE '^[0-9]+' > "$tmp.out"
