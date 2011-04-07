e7a8e9143087bd4afa184579cc528dc9bcacec49
Wu, Ke <wuke@cs.umd.edu>
2011-04-07 10:13:10 -0400

    more smaller regularizors

--- baseline/crfsuite-L1/run_crfsuite_L1.sh
+++ baseline/crfsuite-L1/run_crfsuite_L1.sh
@@ -4,21 +4,21 @@ export PATH="`pwd`/../../tools:$PATH"
 
 declare -i lambda=128
 declare -i max_accuracy=0
-declare -i max_lambda=$lambda
 
-echo "LAMBDA CORRECT" > crf.log
+echo "LAMBDA CORRECT" > crf-L1.log
 
-while [ $lambda -ge 1 ]; do
-    crfsuite learn -m crf.model.$lambda -p regularization=L1 -p regularization.sigma=$lambda train.crfsuite > crf.log.$lambda 2>&1
-    declare -i accuracy=`crfsuite tag -qt -m crf.model.$lambda dev.crfsuite 2>&1 | tee -a crf.log.$lambda | grep '^Item accuracy' | grep -oE '[0-9]+ /' | grep -oE '[0-9]+'`
-    echo $lambda $accuracy | tee -a crf.log
+for  lambda in 128 64 32 16 8 4 3 2 1 0.5 0.25 0.1; do
+    crfsuite learn -m crf-L1.model.$lambda -p regularization=L1 -p regularization.sigma=$lambda train.crfsuite > crf-L1.log.$lambda 2>&1
+    declare -i accuracy=`crfsuite tag -qt -m crf-L1.model.$lambda dev.crfsuite 2>&1 | tee -a crf-L1.log.$lambda | grep '^Item accuracy' | grep -oE '[0-9]+ /' | grep -oE '[0-9]+'`
+    echo $lambda $accuracy | tee -a crf-L1.log
     if [ $accuracy -ge $max_accuracy ]; then
 	max_accuracy=$accuracy
 	max_lambda=$lambda
     fi
     unset accuracy
-    lambda=$(($lambda/2))
 done
 
-crfsuite tag -m crf.model.$max_lambda test.crfsuite > crf.out 2> /dev/null
-crfsuite tag -qt -m crf.model.$max_lambda test.crfsuite > crf.test 2> /dev/null
+echo "Using $max_lambda"
+
+crfsuite tag -m crf-L1.model.$max_lambda test.crfsuite > crf-L1.out 2> /dev/null
+crfsuite tag -qt -m crf-L1.model.$max_lambda test.crfsuite > crf-L1.test 2> /dev/null
--- baseline/crfsuite/run_crfsuite.sh
+++ baseline/crfsuite/run_crfsuite.sh
@@ -2,13 +2,12 @@
 
 export PATH="`pwd`/../../tools:$PATH"
 
-declare -i lambda=128
 declare -i max_accuracy=0
-declare -i max_lambda=$lambda
+declare -i max_lambda=UNKNOWN
 
 echo "LAMBDA CORRECT" > crf.log
 
-while [ $lambda -ge 1 ]; do
+for  lambda in 128 64 32 16 8 4 3 2 1 0.5 0.25 0.1; do
     crfsuite learn -m crf.model.$lambda -p regularization.sigma=$lambda train.crfsuite > crf.log.$lambda 2>&1
     declare -i accuracy=`crfsuite tag -qt -m crf.model.$lambda dev.crfsuite 2>&1 | tee -a crf.log.$lambda | grep '^Item accuracy' | grep -oE '[0-9]+ /' | grep -oE '[0-9]+'`
     echo $lambda $accuracy | tee -a crf.log
@@ -17,8 +16,9 @@ while [ $lambda -ge 1 ]; do
 	max_lambda=$lambda
     fi
     unset accuracy
-    lambda=$(($lambda/2))
 done
+
+echo "Using $max_lambda"
 
 crfsuite tag -m crf.model.$max_lambda test.crfsuite > crf.out 2> /dev/null
 crfsuite tag -qt -m crf.model.$max_lambda test.crfsuite > crf.test 2> /dev/null
