7952c58e2127d2811249ebd3296d4da46525ee31
Wu, Ke <wuke@cs.umd.edu>
2011-04-08 16:59:39 -0400

    report per-tag accuracy

--- eval/accuracy.py
+++ eval/accuracy.py
@@ -2,6 +2,8 @@
 
 import sys
 
+from collections import defaultdict
+
 try:
     ref = open(sys.argv[1])
     ans = open(sys.argv[2])
@@ -15,4 +17,15 @@ ans_vals = [i.split()[0] for i in ans if i.strip()]
 assert len(ref_vals) == len(ans_vals)
 
 correct = sum([i == j for (i, j) in zip(ref_vals, ans_vals)])
-print '{} / {} = {:.2f}%'.format(correct, len(ref_vals), correct * 100. / len(ref_vals))
+print 'Overall: {} / {} = {:.2f}%'.format(correct, len(ref_vals), correct * 100. / len(ref_vals))
+
+per_tag_corret = defaultdict(int)
+per_tag_total = defaultdict(int)
+
+for (r, a) in zip(ref_vals, ans_vals):
+    per_tag_total[r] += 1
+    per_tag_corret[r] += r == a
+
+for t in sorted(set(ref_vals).union(ans_vals)):
+    print '{}: {} / {} = {:.2f}%'.format(t, per_tag_corret[t], per_tag_total[t],
+                                         per_tag_corret[t] * 100. / per_tag_total[t])
