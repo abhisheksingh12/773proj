724bcd9ee5e4d17336873f3f249f5b81a4843679
Wu, Ke <wuke@cs.umd.edu>
2011-04-08 15:37:10 -0400

    add more comments

--- common.py
+++ common.py
@@ -20,6 +20,15 @@ def group_by(items, key):
 
 
 def lazy_load_dyads(lines):
+    """Loading tab-separated data. This return a generator of
+    documents, where each document (of the form [(dyad#, speaker_role,
+    code, thought_unit)]) is the conversation between one dyad. Note
+    that thought_unit is just a single string and you need to split it
+    to get a list of words.
+
+    `lines`: iterable of lines of text
+    """
+
     def item_of(line):
         sp = line.strip().split('\t')
         dyad, _, role, code, unit = sp
@@ -31,11 +40,19 @@ def lazy_load_dyads(lines):
 
 
 def write_dyads(items, fo):
+    """Write items to file.
+
+    `items`: in the same format that lazy_load_dyads outputs
+    `fo`: output file object
+    """
     for (dyad, role, code, unit) in items:
         fo.write('\t'.join(map(str, (dyad, '_', role, code, unit))) + '\n')
 
 
 class IncrCounter:
+    """An increment counter that assigns numbers to things (that is
+    hashable).
+    """
     def __init__(self):
         self.map = {}
 
