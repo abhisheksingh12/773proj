dd97c85237ae7021610ac07d936f10394daf5420
Wu, Ke <wuke@cs.umd.edu>
2011-04-08 14:36:35 -0400

    also record which hyperparameter is used when tuning crf

--- baseline/crfsuite-L1/run_crfsuite_L1.sh
+++ baseline/crfsuite-L1/run_crfsuite_L1.sh
@@ -7,7 +7,7 @@ declare -i max_accuracy=0
 
 echo "LAMBDA CORRECT" > crf-L1.log
 
-for  lambda in 128 64 32 16 8 4 3 2 1 0.5 0.25 0.1; do
+for  lambda in 128 64 32 16 8 4 3 2 1 "0.5" "0.25" "0.1"; do
     crfsuite learn -m crf-L1.model.$lambda -p regularization=L1 -p regularization.sigma=$lambda train.crfsuite > crf-L1.log.$lambda 2>&1
     declare -i accuracy=`crfsuite tag -qt -m crf-L1.model.$lambda dev.crfsuite 2>&1 | tee -a crf-L1.log.$lambda | grep '^Item accuracy' | grep -oE '[0-9]+ /' | grep -oE '[0-9]+'`
     echo $lambda $accuracy | tee -a crf-L1.log
@@ -18,7 +18,7 @@ for  lambda in 128 64 32 16 8 4 3 2 1 0.5 0.25 0.1; do
     unset accuracy
 done
 
-echo "Using $max_lambda"
+echo "Using $max_lambda" | tee -a crf-L1.log
 
 crfsuite tag -m crf-L1.model.$max_lambda test.crfsuite > crf-L1.out 2> /dev/null
 crfsuite tag -qt -m crf-L1.model.$max_lambda test.crfsuite > crf-L1.test 2> /dev/null
--- baseline/crfsuite/run_crfsuite.sh
+++ baseline/crfsuite/run_crfsuite.sh
@@ -7,7 +7,7 @@ declare -i max_lambda=UNKNOWN
 
 echo "LAMBDA CORRECT" > crf.log
 
-for  lambda in 128 64 32 16 8 4 3 2 1 0.5 0.25 0.1; do
+for  lambda in 128 64 32 16 8 4 3 2 1 "0.5" "0.25" "0.1"; do
     crfsuite learn -m crf.model.$lambda -p regularization.sigma=$lambda train.crfsuite > crf.log.$lambda 2>&1
     declare -i accuracy=`crfsuite tag -qt -m crf.model.$lambda dev.crfsuite 2>&1 | tee -a crf.log.$lambda | grep '^Item accuracy' | grep -oE '[0-9]+ /' | grep -oE '[0-9]+'`
     echo $lambda $accuracy | tee -a crf.log
@@ -18,7 +18,7 @@ for  lambda in 128 64 32 16 8 4 3 2 1 0.5 0.25 0.1; do
     unset accuracy
 done
 
-echo "Using $max_lambda"
+echo "Using $max_lambda" | tee -a crf.log
 
 crfsuite tag -m crf.model.$max_lambda test.crfsuite > crf.out 2> /dev/null
 crfsuite tag -qt -m crf.model.$max_lambda test.crfsuite > crf.test 2> /dev/null
