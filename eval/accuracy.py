6d8d5cb8cb390c5b0f315b043090c38d00fa6e75
Wu, Ke <wuke@cs.umd.edu>
2011-04-13 18:54:58 -0400

    evaluating k-best accuracy

--- eval/accuracy.py
+++ eval/accuracy.py
@@ -7,16 +7,18 @@ from collections import defaultdict
 try:
     ref = open(sys.argv[1])
     ans = open(sys.argv[2])
+    sys.argv.append('1')
+    kbest = int(sys.argv[3])
 except:
-    print 'Usage: {} ref ans'.format(sys.argv[0])
+    print 'Usage: {} ref ans [kbest=1]'.format(sys.argv[0])
     exit(1)
 
 ref_vals = [i.split()[0] for i in ref if i.strip()]
-ans_vals = [i.split()[0] for i in ans if i.strip()]
+ans_vals = [set(i.split()[:kbest]) for i in ans if i.strip()]
 
 assert len(ref_vals) == len(ans_vals)
 
-correct = sum([i == j for (i, j) in zip(ref_vals, ans_vals)])
+correct = sum([i in j for (i, j) in zip(ref_vals, ans_vals)])
 print 'Overall: {} / {} = {:.2f}%'.format(correct, len(ref_vals), correct * 100. / len(ref_vals))
 
 per_tag_corret = defaultdict(int)
@@ -24,8 +26,8 @@ per_tag_total = defaultdict(int)
 
 for (r, a) in zip(ref_vals, ans_vals):
     per_tag_total[r] += 1
-    per_tag_corret[r] += r == a
+    per_tag_corret[r] += r in a
 
-for t in sorted(set(ref_vals).union(ans_vals)):
+for t in sorted(set(ref_vals).union(reduce(set.union, ans_vals))):
     print '{}: {} / {} = {:.2f}%'.format(t, per_tag_corret[t], per_tag_total[t],
                                          per_tag_corret[t] * 100. / per_tag_total[t])
