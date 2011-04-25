7178d460c937f894b8e46e61d6b531830fa99466
Wu, Ke <wuke@cs.umd.edu>
2011-04-25 10:10:43 -0400

    separate error rates for train/dev/test

--- maxent/maxent_predict.cc
+++ maxent/maxent_predict.cc
@@ -8,6 +8,29 @@
 
 using namespace std;
 
+bool predict_line(const string &line, const MaxEntModel &m, ostream &output)
+{
+	istringstream strin(line);
+
+	size_t ref_label;
+	strin >> ref_label;
+
+	sparse_t sparse = m.build_sparse(strin);
+
+	vec_t probs;
+	size_t pred_label = m.predict(sparse, probs);
+
+	output << pred_label;
+	for (auto i = probs.begin(); i != probs.end(); ++i)
+		output << (i == probs.begin() ? '\t' : ' ')
+		       << setiosflags(ios_base::fixed)
+		       << setprecision(20)
+		       << *i;
+	output << '\n';
+
+	return pred_label == ref_label;
+}
+
 int main(int argc, char *argv[])
 {
 	if (argc != 2) {
@@ -27,31 +50,40 @@ int main(int argc, char *argv[])
 	string line;
 	size_t correct = 0;
 	size_t total = 0;
-	while (getline(cin, line)) {
-		istringstream strin(line);
-
-		size_t ref_label;
-		strin >> ref_label;
-
-		sparse_t sparse = m.build_sparse(strin);
-
-		vec_t probs;
-		size_t pred_label = m.predict(sparse, probs);
-
-		cout << pred_label;
-		for (auto i = probs.begin(); i != probs.end(); ++i)
-			cout << (i == probs.begin() ? '\t' : ' ')
-			     << setiosflags(ios_base::fixed)
-			     << setprecision(20)
-			     << *i;
-		cout << '\n';
-
-		correct += (pred_label == ref_label);
+	while (getline(cin, line) && line != "DEV" && line != "TEST") {
+		correct += predict_line(line, m, cout);
 		++total;
 	}
 	cerr << "Error rate = "
 	     << (total - correct) << " / " << total
 	     << " = " << (total - correct) / static_cast<double>(total)
 	     << endl;
+
+	if (line == "DEV") {
+		correct = 0;
+		total = 0;
+		while (getline(cin, line) && line != "TEST") {
+			correct += predict_line(line, m, cout);
+			++total;
+		}
+		cerr << "Dev error rate = "
+		     << (total - correct) << " / " << total
+		     << " = " << (total - correct) / static_cast<double>(total)
+		     << endl;
+	}
+
+	if (line == "TEST") {
+		correct = 0;
+		total = 0;
+		while (getline(cin, line)) {
+			correct += predict_line(line, m, cout);
+			++total;
+		}
+		cerr << "Test error rate = "
+		     << (total - correct) << " / " << total
+		     << " = " << (total - correct) / static_cast<double>(total)
+		     << endl;
+	}
+
 	return 0;
 }
