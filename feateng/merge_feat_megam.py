dd1e26a83a20adbf419fe3342ff1e7995740f62e
Wu, Ke <wuke@cs.umd.edu>
2011-04-30 16:37:13 -0400

    merge features

---
+++ feateng/merge_feat_megam.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python2.7
+
+"""Merge feature files"""
+
+import sys
+
+from itertools import izip
+
+
+def merge_feats(feats):
+    labels = set([l for l,_ in feats])
+    if len(labels) != 1:
+        print labels
+        assert False
+    label = list(labels)[0]
+    feat = list(reduce(set.union, [f for _,f in feats], set()))
+    return label, feat
+
+
+def feat_of_line(line):
+    label, feats = line.split(None, 1)
+    return label, feats.split()
+
+
+if __name__ == '__main__':
+    try:
+        output_path = sys.argv[1]
+        input_paths = sys.argv[2:]
+    except:
+        print >> sys.stderr, "Usage: {} output_path input_path [input_paths ...]".format(sys.argv[0])
+        raise
+
+    fo = open(output_path, 'w')
+    fis = map(open, input_paths)
+
+    for lines in izip(*fis):
+        label, feat = merge_feats(map(feat_of_line, lines))
+        fo.write('{}\t{}\n'.format(
+            label,
+            ' '.join(feat)))
+
+    for f in fis:
+        f.close()
+    fo.close()
